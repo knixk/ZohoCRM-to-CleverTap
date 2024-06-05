5e9fc8ab27d6ef97b64ed5009aebd50da5362b29
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2024-06-05 12:31:54 +0530

    first good commit

--- pipeline.js
+++ pipeline.js
@@ -1,14 +1,107 @@
+require('dotenv').config();
+const axios = require("axios");
 const express = require("express");
 const app = express();
 const port = 3000;
-// const cors = require("cors");
 
+const ctAccountId = process.env.CLEVERTAP_ACCOUNT_ID;
+const ctPasscode = process.env.CLEVERTAP_PASSCODE;
+const region = process.env.CLEVERTAP_REGION;
+
+
+// const cors = require("cors");
 // app.use(cors);
 
+// First generate the o-auth 2.0 token and then you'll get the access token used to make api calls 
+
+const zohoUsers = 'https://www.zohoapis.in/crm/v6/users?type=AllUsers';
+const authTokenForZohoUsers = process.env.AUTH_TOKEN_ZOHO_USERS_MODULES_SCOPE;
+
+// console.log(ctAccountId, ctPasscode, region, authTokenForZohoUsers);
+
+const fetchZohoUsers = async () => {
+
+  try {
+        const response = await axios.get(zohoUsers, {
+            headers: {
+                'Authorization': `Zoho-oauthtoken ${authTokenForZohoUsers}`
+            }
+        });
+
+        console.log(response.data);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching users:', error);
+        throw error;  // Rethrow the error to handle it further up the call stack if needed
+    }
+}
+
+const pushToCleverTap = async (profile) => {
+
+  const headers = {
+        'X-CleverTap-Account-Id': ctAccountId,
+        'X-CleverTap-Passcode': ctPasscode,
+        'Content-Type': 'application/json; charset=utf-8'
+    };
+
+    const data = {
+        d: [profile]
+    };
+
+    try { 
+        const response = await axios.post(url, data, { headers });
+        // console.log(response.data)
+        return response.data;
+    } catch (error) {
+        console.error('Error creating/updating user profile:', error.response ? error.response.data : error.message);
+        throw error;
+    }
+}
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+
+  const callAPIs = async () => {
+    const data = await fetchZohoUsers();
+    const {country, state, zuid, email, full_name, id} = data.users[0]
+    // console.log(country, state, zuid, email, full_name, id, "data using asd");
+
+    const profile = {
+      objectId: id, // Unique identifier for the user
+      type: "profile",
+      profileData: {
+          Name: full_name,
+          Email: email,
+          "country": country,
+          "state": state,
+          "zuid": zuid
+      }
+    };  
+
+    const response = pushToCleverTap(profile);
+    // console.log('User profile created/updated successfully:', response);
+
+  }
+
+  callAPIs();
+
+  // const data = fetchZohoUsers();
+  // const nested = data.users;
+  // console.log(country, state, zuid, email, full_name, id);
+  res.send('Pipeline running...');
+
+
+
 });
+    
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+
+
+// to do:
+
+// get the zoho deals access
+
+// try to push some perso data to clevertap
